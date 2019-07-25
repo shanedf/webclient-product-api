b4472e817530affe8b3b077e43cc59ffbf0153e2
lhr-shanef <shane.feather@expeditors.com>
2019-07-25 09:34:32 +0100

    api client complete

--- src/main/java/ProductEvent.java
+++ src/main/java/ProductEvent.java
@@ -1,2 +1,14 @@
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class ProductEvent {
+
+    private Long eventId;
+
+    private String eventType;
+
 }
